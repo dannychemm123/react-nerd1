dd2fce69493377abd5c5dbb470ccc5dd65be0e87
dannychemm123 <dannychemm121@gmail.com>
2024-06-14 13:27:16 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> 
+    <User/>
+    <User/>
+    <User/>
+    </div>
+  );
+}
+
+const User = ()=>{
+  return (
+    <div>
+      <h1>Danny</h1>
+      <h1>24</h1>
+      <h1>danny@danny.com</h1>
+    </div>
+  )
+}
+
+export default App;
