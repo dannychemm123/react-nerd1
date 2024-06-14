f6acfd4a7f8699fb86039e16aa80941649f254a7
dannychemm123 <dannychemm121@gmail.com>
2024-06-14 15:56:11 +0000

    third commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,10 @@
 import './App.css';
 
 function App() {
-    const planets = [
-      { name: 'Mars', isGasPlanet: false },
-      { name: 'Earth', isGasPlanet: false},
-      { name: 'Jupiter', isGasPlanet: true },
-      { name: 'Saturn', isGasPlanet: true},
-      { name: 'Venus', isGasPlanet: false},
-      { name: 'Neptune', isGasPlanet: true},
-      { name: 'Uranus', isGasPlanet: true},
-    ]
+    
   return (
     <div className="App"> 
-    {planets.map((planets,key)=>{
-      if (planets.isGasPlanet) return <div>{planets.name}</div>
-    }
-
-  )}
-    
+    <h1>Hello World!</h1>
     </div>
   );
 }
--- src/experiment.js
+++ src/experiment.js
@@ -37,3 +37,34 @@ const User = (props)=>{
   )
  }
 export default App;
+
+{/* 
+    
+import './App.css';
+
+function App() {
+    const planets = [
+      { name: 'Mars', isGasPlanet: false },
+      { name: 'Earth', isGasPlanet: false},
+      { name: 'Jupiter', isGasPlanet: true },
+      { name: 'Saturn', isGasPlanet: true},
+      { name: 'Venus', isGasPlanet: false},
+      { name: 'Neptune', isGasPlanet: true},
+      { name: 'Uranus', isGasPlanet: true},
+    ]
+  return (
+    <div className="App"> 
+    {planets.map((planets,key)=>{
+      if (planets.isGasPlanet) return <h1>{planets.name}</h1>
+    }
+
+  )}
+    
+    </div>
+  );
+}
+
+
+export default App;
+
+    */}
